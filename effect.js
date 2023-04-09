c0b6a211cda8a9bde71c89a8b8359ecc749a984b
ShreyW <lightmaths2357@gmail.com>
2023-04-09 20:43:16 +0530

    first commmit

---
+++ effect.js
@@ -0,0 +1,40 @@
+$(window).load(function(){
+	// $('.loading').fadeOut('fast');
+	$('.container').fadeIn('fast');
+});
+$('document').ready(function(){
+	var vw;
+		$(window).resize(function(){
+			 vw = $(window).width()/2;
+		});
+
+	$('#turn_on').click(function(){
+		$('#bulb_yellow').addClass('bulb-glow-yellow');
+		$('#bulb_red').addClass('bulb-glow-red');
+		$('#bulb_blue').addClass('bulb-glow-blue');
+		$('#bulb_green').addClass('bulb-glow-green');
+		$('#bulb_pink').addClass('bulb-glow-pink');
+		$('#bulb_orange').addClass('bulb-glow-orange');
+		$('body').addClass('peach');
+		$(this).fadeOut('slow').delay(4000).promise().done(function(){
+			$('#play').fadeIn('slow');
+		});
+	});
+	$('#play').click(function(){
+		var audio = $('.song')[0];
+        audio.play();
+        $('#bulb_yellow').addClass('bulb-glow-yellow-after');
+		$('#bulb_red').addClass('bulb-glow-red-after');
+		$('#bulb_blue').addClass('bulb-glow-blue-after');
+		$('#bulb_green').addClass('bulb-glow-green-after');
+		$('#bulb_pink').addClass('bulb-glow-pink-after');
+		$('#bulb_orange').addClass('bulb-glow-orange-after');
+		setTimeout(function(){
+			$("#secret").addClass("loading");
+		}, 8000);
+		$('body').css('backgroud-color','#FFF');
+		$('body').addClass('peach-after');
+	});
+	
+});
+
